17f6f06027cbfa34eb3bb9352008c41927dfbe94
thetandoori <Rahul@nbp-148-37.nbp.ruw.rutgers.edu>
2015-04-13 16:57:52 -0400

    Got rid of tests

--- Main.java
+++ Main.java
@@ -7,15 +7,7 @@ public class Main {
 
 	public static void main(String[] args)
 	{
-		//Used for testing till database is fully functioning.
-		ExampleOrders test=new ExampleOrders();
-		KitchenStaffHandler.addTableOrder(test.table2);
-		KitchenStaffHandler.addTableOrder(test.table3);
-		KitchenStaffHandler.addTableOrder(test.table5);
-		KitchenStaffHandler.addTableOrder(test.table4);
-		KitchenStaffHandler.addTableOrder(test.table1);
-		
-		
+
 		
 		new LoginWindow();	
 
